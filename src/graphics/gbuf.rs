c1be8cc32ecfd038225bfa08c41c23cd0b5e6875
jhosein58 <jhosein58@gmail.com>
2025-07-16 04:14:36 +0330

    graphical buffer added

---
+++ src/graphics/gbuf.rs
@@ -0,0 +1,47 @@
+pub struct GBuf{
+    buf: Vec<u32>,
+    def_bg: u32,
+    w: usize,
+    h: usize
+}
+
+impl GBuf {
+
+    pub fn new(w: usize, h: usize, c: u32) -> Self {
+        
+        let size: usize = w * h;
+        let mut buf = Vec::with_capacity(size);
+        buf.resize(size, c);
+
+        Self {
+            buf,
+            def_bg: c,
+            w,
+            h
+        }
+    }
+
+    pub fn resize(&mut self, w: usize, h: usize) {
+        let new_len = w * h;
+        if self.buf.len() != new_len {
+            self.buf.resize(new_len, self.def_bg);
+        }
+        self.w = w;
+        self.h = h;
+    }
+
+    pub fn resize_if_needed(&mut self, w: usize, h: usize) {
+        if self.w != w || self.h != h {
+            self.resize(w, h);
+        }
+    }
+
+    #[inline]
+    pub fn size(&self) -> (usize, usize) {
+        (self.w, self.h)
+    }
+
+    pub fn flush(&mut self) {
+        self.buf.fill(self.def_bg);
+    }
+}
--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -1,2 +1,3 @@
 pub mod screen;
 pub mod color;
+pub mod gbuf;
